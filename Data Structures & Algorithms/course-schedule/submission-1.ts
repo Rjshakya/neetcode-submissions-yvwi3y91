16c5246db1be7c83cc684b25f88a6545d9beeb16
Raj shakya <110652481+Rjshakya@users.noreply.github.com>
2026-06-10 14:50:00 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.ts
@@ -0,0 +1,62 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses: number, prerequisites: number[][]): boolean {
+
+
+        const graphAdjList:Record<number , number[]> = {}
+        /**
+         * 
+         * {
+         *  1 : [0],
+         *  0 : [1]
+         * }
+         */
+
+        for (let i = 0; i < numCourses; i++){
+            graphAdjList[i] = []
+        }
+
+        for (const [a, b] of prerequisites) {
+            graphAdjList[a].push(b)
+        }
+
+        const state = new Array(numCourses).fill(0)
+        /**
+         * 
+         * 0 - intial state
+         * 1 - visited state 
+         * 2 - done state
+         */
+
+        function dfs(node:number){
+            // having state 1 of node , in starting
+            // it means it was already , and we are visiting it again
+            // it is cycle detecting.
+            if(state[node] === 1) return false
+            // state 2 says , all processing is done  node
+            if(state[node] === 2) return true
+
+            state[node] = 1
+
+            for (const neighbor of graphAdjList[node]){
+                if(!dfs(neighbor)) return false
+            }
+
+            state[node] = 2
+            return true
+
+        }
+
+        for(let i = 0 ; i < numCourses; i++){
+            if(!dfs(i))return false
+        }
+
+
+        return true
+
+    }
+}
