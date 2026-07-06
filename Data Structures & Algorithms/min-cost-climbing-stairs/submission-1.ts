032a77113d752c45ae9f89791f46274fa37f6273
Raj shakya <110652481+Rjshakya@users.noreply.github.com>
2026-07-07 02:47:14 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost: number[]): number {
+
+        
+        const memo:Map<number,number> = new Map()
+        function dfs(i:number ){
+            if(i >= cost.length)return 0;
+            if(memo.has(i))return memo.get(i)
+
+            const result = cost[i] + Math.min(dfs(i+1) , dfs(i+2))
+            memo.set(i,result)
+            return result
+
+        }
+
+        return Math.min(dfs(0) , dfs(1))
+
+    }
+}
