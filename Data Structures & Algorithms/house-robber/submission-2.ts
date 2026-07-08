b02c33cd28d116236469b1b7e34fa20d308a39b6
Raj shakya <110652481+Rjshakya@users.noreply.github.com>
2026-07-09 01:57:42 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.ts
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums: number[]): number {
+
+        /**
+         * 
+         * nums[i] (value at index) = amount of money
+         * i (index) = house
+         * 
+         *  can't go
+         *  - i + 1 or i - 1
+         * 
+         * max money i can rob , 
+         * 
+         * lets start with 0 house ,
+         * 
+         *   - from each index we have to two choices , we can't go to 
+         *   - i + 1 house . or i - 1 house
+         * 
+         * 
+         * 
+         * 
+         */
+
+        const map = new Map()
+        function dfs(i: number) {
+            if (i >= nums.length) return 0
+            if (map.has(i)) return map.get(i)
+            const cost = Math.max(nums[i] + dfs(i + 2) , dfs(i+1))
+            map.set(i, cost)
+            return cost
+        }
+
+        let max = -Infinity
+
+        for (let i = 0; i < nums.length; i++) {
+            max = Math.max(dfs(i) , max)
+        }
+        return max
+
+    }
+}
