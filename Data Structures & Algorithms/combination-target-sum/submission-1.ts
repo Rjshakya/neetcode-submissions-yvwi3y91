bfd562e0ffaa8307d655913a286d3d6e54960361
Raj shakya <110652481+Rjshakya@users.noreply.github.com>
2026-08-09 23:26:16 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums: number[], target: number): number[][] {
+
+        const res : number[][] = []
+
+        function dfs(i , sub , total){
+
+            if(total === target){
+                res.push([...sub])
+                return ;
+            }
+
+            if (i >= nums.length || total > target) return;
+
+            sub.push(nums[i])
+            dfs(i , sub , total + nums[i])
+
+            sub.pop()
+            dfs(i+1 , sub , total)
+        }
+
+        dfs(0,[],0)
+        return res
+
+    }
+}
