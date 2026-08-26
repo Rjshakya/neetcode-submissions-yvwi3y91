d8cfd80ab91df0be3ce291d5dd8be4ca38ae8d8c
Raj shakya <110652481+Rjshakya@users.noreply.github.com>
2026-08-26 14:26:06 +0530

    Add: find-duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-3.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums: number[]): number {
+
+        let s = 0
+        let f = 0
+
+        while(true){
+            s = nums[s]
+            f = nums[nums[f]]
+
+            if(s === f){
+                break;
+            }
+
+        }
+
+
+        let s2 = 0
+        while(true){
+            s = nums[s]
+            s2 = nums[s2]
+
+            if(s === s2){
+                break;
+            }
+        }
+
+        return s2
+
+    }
+}
